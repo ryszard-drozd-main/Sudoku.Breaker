9b7c76fcae8b60182065c744a08ed6acca1c14ed
Ryszard Drozd <csharp@poczta.onet.pl>
2020-09-02 14:50:42 +0200

    remove ocr and form

--- Sudoku.Breaker.ImageOcr/Services/IProcessImage.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Drawing;
-
-namespace Sudoku.Breaker.ImageOcr.Services
-{
-    public interface IProcessImage
-    {
-        string Process(string path, Rectangle rect, Graphics g); // Tesseract
-    }
-}
